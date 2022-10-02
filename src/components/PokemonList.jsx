de96caf4912132821756a11392922de404f3ad3a
yuxianxu <yuxian.xu@gmail.com>
2022-10-01 23:24:28 -0700

    Add notes for no result page

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -8,18 +8,21 @@ function PokemonList() {
     state: { pokemon, search, modal },
   } = useContext(PokemonContext);
 
+  const filteredPokemon = pokemon.filter(({ name: { english } }) =>
+    english.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+  );
+
   return (
     <>
-      {pokemon.length == 0 && <p>Loading...</p>}
+      {pokemon.length == 0 && <p className="empty">Loading...</p>}
+      {filteredPokemon.length > 0 && <p className="filterResults">{filteredPokemon.length} result{filteredPokemon.length > 1 ? 's' : ''} found</p>}
       {modal && <PokemonSelected />}
-      {pokemon
-        .filter(({ name: { english } }) =>
-          english.toLocaleLowerCase().includes(search.toLocaleLowerCase())
-        )
-        .slice(0, 20)
-        .map((pokemon) => (
-          <Pokemon pokemon={pokemon} key={pokemon.id} />
-        ))}
+      {filteredPokemon.slice(0, 20).map((pokemon) => (
+        <Pokemon pokemon={pokemon} key={pokemon.id} />
+      ))}
+      {filteredPokemon.length < 1 && (
+        <p className="empty">Ohh, no pokemon found</p>
+      )}
     </>
   );
 }
